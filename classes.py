a891958fe62a2e0dea88678bd1f2813a2ac1a993
James Tan <jamestancheemin98@gmail.com>
2018-09-09 13:58:54 +0800

    improved algorith, cute printouts

--- classes.py
+++ classes.py
@@ -1,15 +1,85 @@
-class desert:
+UNLIMITED = 999999999
+desertCampList=[]
+
+class Truck:
+	oilCount = 0
+	id = 0
+	moves = 0
+
+	def pickup(self,count):
+		desertCampList[self.id].minus(count)
+		self.oilCount += count
+
+	def unload(self):
+		desertCampList[self.id].add(self.oilCount)
+		self.oilCount = 0
+
+	def move(self,direction):
+		if(self.oilCount<0):
+			raise Exception("OI NO OIL LA")
+		self.moves += 1
+		if(direction):
+			self.pickup(min(3,desertCampList[self.id].oilCount)) #picks up all available oil
+			self.id += 1
+		else:
+			self.pickup(1)
+			self.id -= 1
+		self.oilCount -= 1
+		self.unload()
+		# print("Bob is currently at " + str(self.id))
+
+
+class DesertCamp:
+
+	oilCount = 0
+	id = 0
+	totalOilCount = 0
 
 	def __init__(self,id,oilCount=0):
 		self.oilCount = oilCount
 		self.id = id
 
-	def drop(self,count):
+	def add(self,count):
 		self.oilCount += count
+		self.totalOilCount = max(self.totalOilCount,self.oilCount)
+		if(self.oilCount<0):
+			raise Exception('ERROR SIAL CAMP LESS THAN 0')
 
-	def pickup(self,count):
-		self.oilCount -+ count
+	def minus(self,count):
+		self.oilCount -= count
+		if(self.oilCount<0):
+			raise Exception('ERROR SIAL CAMP LESS THAN 0')
 
-def start(n,desertCamp):
-	for i in range(0,n):
-		desertCamp.append(desert(i))
+def start(n,desertCampList): #initialize camps
+	print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n ............. THE TRUCK IS MOVING ................\n\n\n\n\n\n\n\n")
+	desertCampList.append(DesertCamp(0,UNLIMITED))
+	for i in range(1,n+1):
+		desertCampList.append(DesertCamp(i))
+
+def checkWinningMove(truck,n,desertCampList): #returns true if the truck can go to target camp
+	if((n - truck.id)==3 & desertCampList[truck.id].oilCount>=3):
+		print("difference : "+str(n - truck.id) +" oilCount " + str(desertCampList[truck.id].oilCount))
+		return True
+	else:
+		return False
+
+def checkSufficient(winningCondition,bob,desertCampList): #returns true if the total oil count to get to target camp is hit
+	if(desertCampList[1].oilCount>=winningCondition[1]):
+		return True
+	else:
+		return False
+
+
+def checkWin(truck,n): #returns true if truck is at target camp
+	if (truck.id == n):
+		print("TOTAL MOVES = " + str(truck.moves) + "\nYOU WON !\n")
+		return True
+	else:
+		return False
+
+def setWinningCondition(n,winningCondition): 	#calculates the total oil count requirement at each camp to get to target camp
+	for i in range(n-2,n+1,1):
+		winningCondition[i] = 0
+	winningCondition[n-3] = 3
+	for i in range(n-4,0,-1):
+		winningCondition[i]=(winningCondition[i+1]*3) - 1
--- main.py
+++ main.py
@@ -1,122 +1,51 @@
-###########################
-
-# 1. Algorithm retakes the load even though it is not needed
-
-###########################
 from classes import *
 
-UNLIMITED = 999999999
-n = int(input("Enter n: "))
-desertCampList=[]
-ready = 0
+
+ready = False
 
 ##############
 # 3 is needed to have an effective move, 1 to go, 1 to store, 1 to come back
-##############
-
+#
+#
 # DIRECTION #
 # 0 = left  #
 # 1 = right #
+#
+##############
 
+
+n = int(input("Enter n: "))
 winningCondition=[0]*(n+1) #creates an array to dynamically calculate the amount of oil required
-for i in range(n-2,n+1,1):
-	winningCondition[i] = 0
-winningCondition[n-3] = 3
-for i in range(n-4,0,-1):
-	winningCondition[i]=(winningCondition[i+1]*3) - 1
 
-class Truck:
-	oilCount = 0
-	id = 0
-	moves = 0
-
-	def pickup(self,count):
-		desertCampList[self.id].minus(count)
-		self.oilCount += count
-
-	def unload(self):
-		desertCampList[self.id].add(self.oilCount)
-		self.oilCount = 0
-
-	def move(self,direction):
-		if(self.oilCount<0):
-			print("OI NO OIL LA SIAL")
-		self.moves += 1
-		if(direction):
-			self.pickup(min(3,desertCampList[self.id].oilCount)) #picks up all available oil
-			self.id += 1
-		else:
-			self.pickup(1)
-			self.id -= 1
-		self.oilCount -= 1
-		self.unload()
-		print("Bob is currently at " + str(self.id))
-
-
-class DesertCamp:
-
-	oilCount = 0
-	id = 0
-	totalOilCount = 0
-
-	def __init__(self,id,oilCount=0):
-		self.oilCount = oilCount
-		self.id = id
-
-	def add(self,count):
-		self.oilCount += count
-		self.totalOilCount += count
-		if(self.oilCount<0):
-			raise Exception('ERROR SIAL CAMP LESS THAN 0')
-
-	def minus(self,count):
-		self.oilCount -= count
-		if(self.oilCount<0):
-			raise Exception('ERROR SIAL CAMP LESS THAN 0')
-
-def start(n,desertCampList):
-	desertCampList.append(DesertCamp(0,UNLIMITED))
-	for i in range(1,n+1):
-		desertCampList.append(DesertCamp(i))
-
-def checkWinningMove(truck,n,desertCampList):
-	if((n - truck.id)==3 & desertCampList[truck.id].oilCount>=3):
-		print("difference : "+str(n - truck.id) +" oilCount " + str(desertCampList[truck.id].oilCount))
-		truck.move(1)
-		truck.move(1)
-		truck.move(1)
-
-def checkSufficient(n,desertCamp,id):
-	print("ID :" + str(id) + "only has count: " + str(desertCamp.oilCount))
-	# if(desertCamp.oilCount >= (3+id)): #ensures that truck will have enough to traverse back to base camp, bruteforce technique
-	if(desertCamp.oilCount >= (3**(n-id-2)-1)): #more efficient algorithm but DOESN'T MAKE IT FASTER???
-		return True
-	else:
-		return False
-
-def checkWin(truck,n):
-	if (truck.id == n):
-		print("TOTAL MOVES = " + str(truck.moves) + "\nYOU WON !\n")
-
-		return True
-	else:
-		return False
-
+setWinningCondition(n,winningCondition)
 start(n,desertCampList)
 bob = Truck()
-
+# for i in range(n+1):
+# 	print(winningCondition[i])
 
 
 while(1):
-	checkWinningMove(bob,n,desertCampList)
-	if(checkWin(bob,n)):
-		for i in range(n):
-			print("ID :" + str(i) + "only has total count: " + str(desertCampList[i].totalOilCount))
-		break
-	elif(checkSufficient(n,desertCampList[bob.id],bob.id)):
-		ready = 1
+	if(checkWinningMove(bob,n,desertCampList)): #checks for winning move and executes
 		bob.move(1)
+		bob.move(1)
+		bob.move(1)
+		if(checkWin(bob,n)):
+			for i in range(n):
+				print("ID :" + str(i) + "only has total count: " + str(desertCampList[i].totalOilCount))
+		break
+	if(ready == True):
+		while(desertCampList[bob.id].oilCount>3): #moves all oil to next desert camp
+			bob.move(1)
+			bob.move(0)
+		bob.move(1)  #moves oil to desert camp without coming back
 	else:
-		bob.move(0)
+		bob.move(1)
+		if(checkSufficient(winningCondition,bob,desertCampList)==True): #if oil is sufficient for truck to traverse till the end
+			ready = True
+		else:
+			# time.sleep(10)
+			bob.move(0)
+
+
 
 
